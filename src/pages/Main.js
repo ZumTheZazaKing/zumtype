b1b5be32e748407a7059dd13a4d60f3de596ce59
ZumTheZazaKing <zahidi85543@gmail.com>
2022-08-23 08:56:01 +0800

    key detection and checking added. The paragraph will be renewed if typer finishes it but checking styles still remains

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,26 +1,84 @@
-import { useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { useState, useEffect, useRef } from "react";
 
 export const Main = () => {
 
-    const [fetchedPara, setFetchedPara] = useState("");
+    const [sentences, setSentences] = useState(JSON.parse(localStorage.getItem("sentences")) || null);
+    const acceptedKeys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,;.!?()[]{}/"\'Backspace'
+    const [chosenSentences, setChosenSentences] = useState("");
+    let typed = [];
+    let wrong = 0;
+    const paraRef = useRef();
 
-    fetch("http://metaphorpsum.com/sentences/5")
-    .then(res => res.text())
-    .then(data => console.log(data))
+    useEffect(() => {
+        if(!sentences){
+            fetch("http://metaphorpsum.com/sentences/1000000")
+            .then(res => res.text())
+            .then(data => localStorage.setItem("sentences",JSON.stringify(data.split(". "))))
+        }
+
+        document.addEventListener('keydown',detectKeyDown,true);
+
+    },[])
+
+    useEffect(()=>{
+       if(!sentences)return;
+       const start = Math.floor(Math.random()*(sentences.length-5))
+       setChosenSentences((sentences.slice(start, start+4)).join(". "))
+    },[sentences])
+
+    const detectKeyDown = e => {
+        if(!acceptedKeys.includes(e.key))return;
+        if(e.key === "Backspace"){
+            if(paraRef.current.children[typed.length-1].classList.contains('wrong')){
+                wrong--;
+            }
+            if(paraRef.current.children[typed.length-1].innerHTML===" ")typed.pop();
+            typed.pop();
+            paraRef.current.children[typed.length].classList.remove('wrong');
+            paraRef.current.children[typed.length].classList.remove('polish');
+            paraRef.current.children[typed.length].classList.add('unpolish');
+            if(typed[typed.length-1]===" "){typed.pop();}
+        }else{
+            if(typed.length===[...paraRef.current.children].length)return;
+            if(paraRef.current.children[typed.length].innerHTML===" ")typed.push(" ");
+            typed.push(e.key)
+            if(typed[typed.length-1]===paraRef.current.children[typed.length-1].innerHTML){
+                paraRef.current.children[typed.length-1].classList.remove('unpolish');
+                paraRef.current.children[typed.length-1].classList.add('polish');
+                if(typed.length===[...paraRef.current.children].length && wrong===0){
+                    const start = Math.floor(Math.random()*(sentences.length-5))
+                    setChosenSentences((sentences.slice(start, start+4)).join(". "))
+                }
+            }else{
+                paraRef.current.children[typed.length-1].classList.remove('unpolish');
+                paraRef.current.children[typed.length-1].classList.add('wrong');
+                wrong++;
+            }
+            if(paraRef.current.children[typed.length].innerHTML===" "){
+                typed.push(" ")
+            }
+        }
+        
+        console.log(typed);
+
+    }
 
     return (
         <div 
-            className="
-                border border-black h-screen w-screen flex items-center
+            className="d
+                h-screen w-screen flex items-center
                 justify-center font-mono flex-col
             "
         >
-            <p className="accent text-xl">
+            <p className="accent sm:text-xl cursor-default">
                 Start Typing
             </p>
             <br/>
-            <p className="text-3xl secondary">
-                Main
+            <p ref={paraRef} className="sm:text-3xl text-justify px-10 w-screen sm:w-3/4 leading-normal">
+                {[...chosenSentences].map((sentence,i) => 
+                    <span className={`unpolish`} key={i}>{sentence}</span>
+                )}
             </p>
         </div>
     )
